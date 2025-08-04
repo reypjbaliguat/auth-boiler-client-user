297d4cb95228f4dabb7698945caa31093da13a1d
reypjbaliguat <reypjbaliguat@gmail.com>
2025-08-04 20:53:08 +0800

    fix(register): update route and headers

--- src/pages/Register/components/RegisterForm.tsx
+++ src/pages/Register/components/RegisterForm.tsx
@@ -20,8 +20,11 @@ function RegisterForm({ handleSubmitForm, error }: Props) {
     const handleGoogleSuccess = async (
         credentialResponse: CredentialResponse,
     ) => {
-        const res = await fetch('http://localhost:5000/api/auth/google', {
+        const res = await fetch('http://localhost:5000/v1/api/auth/google', {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
             body: JSON.stringify({
                 credential: credentialResponse.credential,
             }),
